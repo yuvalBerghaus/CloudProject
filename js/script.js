6b0611ab25266fad85b6ce7a4438c9210768242a
Lotem <48315369+LotemY@users.noreply.github.com>
2021-05-01 16:10:16 +0300

    form

--- js/script.js
+++ js/script.js
@@ -1,28 +1,81 @@
+let carArr = [];
+let memArr = [];
 
-fetch("./users.json").then(res => {
-    return res.json();
-}).then(arr => {
-    for (i in arr) {
-        for (j of arr[i]) {
-            if (j.name) {
-                document.getElementById(i).innerHTML +=
-                    `<li class="rectangle" name="myParkingLots[]">
-                <article>
-                    <img class="user" src="${j.img}">
-                </article>
-                <h2>${i == 'my' ? 'My' : j.name + '\'s'} Parking</h2>
-                <section class="statusBox">
-                    <article class="trafficLight" id="${j.status}">
-                    </article>
-                    <span>${j.status == 'Attention' ? j.status + '!' : j.status}</span>
-                </section>
-                </li>
-                `;
+onload = () => {
+    fetch("../json/users.json").then(res => {
+        return res.json();
+    }).then(arr => {
+        for (i in arr) {
+            for (j of arr[i]) {
+                if (j.name) {
+                    document.getElementById(i).innerHTML +=
+                        `<li class="rectangle" name="myParkingLots[]">
+                            <button onclick="${i == 'my' ? 'my' : j.name}Parking()">
+                            <article>
+                                <img class="user" src="${j.img}">
+                            </article>
+                            <h2>${i == 'my' ? 'My' : j.name + '\'s'} Parking</h2>
+                            <section class="statusBox">
+                                <article class="trafficLight" id="${j.status}"></article>
+                                <span>${j.status == 'Attention' ? j.status + '!' : j.status}</span>
+                            </section>
+                            </button>
+                        </li>`;
+                }
+                else
+                    document.getElementById(i).innerHTML += `<li id="none">NONE</li>`;
             }
-            else {
-                document.getElementById(i).innerHTML += `<li id="none">NONE</li>`;
-            }
+            if (i != "friends")
+                document.getElementById(i).innerHTML += `<hr class="line">`;
         }
-        document.getElementById(i).innerHTML += `<hr class="line">`;
+    });
+}
+
+myParking = () => {
+    location.href = "parkingPage.html";
+}
+
+TomParking = () => {
+    location.href = "#";
+}
+
+YoniParking = () => {
+    location.href = "#";
+}
+
+openForm = () => {
+    document.getElementById("formOverlay").style.display = "block";
+}
+
+closeForm = () => {
+    document.getElementById("formOverlay").style.display = "none";
+}
+
+addElement = (arr) => {
+    if (document.getElementById(arr).value) {
+        if (arr == "cars[]") {
+            carArr[carArr.length] = document.getElementById(arr).value;
+            document.getElementById(arr).value = "";
+        }
+        viewCars();
     }
-});
+}
+
+addMember = () => {
+    if (document.getElementById("memb").value) {
+        memArr[memArr.length] = [document.getElementById("memb").value, document.getElementById("op").value];
+        document.getElementById("memb").value = "";
+        document.getElementById("addedMem").innerHTML = `<h6>${memArr}</h6>`;
+    }
+}
+
+viewCars = () => {
+    document.getElementById("addedCars").innerHTML = "";
+    for (i in carArr)
+        document.getElementById("addedCars").innerHTML += `<article class="carCube">${carArr[i]}<img id="car${i}" src="images/trash.svg" onclick="removeCar(this.id)"></article>`;
+}
+
+removeCar = (index) => {
+    carArr.splice(index.split('car')[1], 1);
+    viewCars();
+}
