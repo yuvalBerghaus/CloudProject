2f0725d37299629ad78904e5c6f7e8f45f72ddb8
yuvalBerghaus <markberghaus@outlook.com>
2021-05-01 16:06:20 +0300

    Leh tizdayen

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
-fetch("../users.json").then(res => {
+
+fetch("./users.json").then(res => {
     return res.json();
 }).then(arr => {
-    for (i in arr)
+    for (i in arr) {
         for (j of arr[i]) {
             if (j.name) {
                 document.getElementById(i).innerHTML +=
                     `<li class="rectangle" name="myParkingLots[]">
-            <article>
-                <img class="user" src="${j.img}">
-            </article>
-            <h2>${i == 'my' ? 'My' : j.name + '\'s'} Parking</h2>
-            <section class="statusBox">
-                <article class="trafficLight" id="${j.status}">
+                <article>
+                    <img class="user" src="${j.img}">
                 </article>
-                <span>${j.status == 'Attention' ? j.status + '!' : j.status}</span>
-            </section>
-            </li>
-            <hr class="line">`;
+                <h2>${i == 'my' ? 'My' : j.name + '\'s'} Parking</h2>
+                <section class="statusBox">
+                    <article class="trafficLight" id="${j.status}">
+                    </article>
+                    <span>${j.status == 'Attention' ? j.status + '!' : j.status}</span>
+                </section>
+                </li>
+                `;
             }
             else {
                 document.getElementById(i).innerHTML += `<li id="none">NONE</li>`;
             }
         }
+        document.getElementById(i).innerHTML += `<hr class="line">`;
+    }
 });
