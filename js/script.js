5c0351f383b17d3c5109ab80c1c4681657545464
Lotem <48315369+LotemY@users.noreply.github.com>
2021-05-02 14:55:24 +0300

    new json and carInfo

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 let carArr = [];
 let memArr = [];
 
-onload = () => {
-    fetch("../json/users.json").then(res => {
-        return res.json();
-    }).then(arr => {
+list = () => fetch("../json/users.json").then(res => {
+    return res.json();
+}).then(arr => {
+    try {
         for (i in arr) {
             for (j of arr[i]) {
                 if (j.name) {
                     document.getElementById(i).innerHTML +=
                         `<li class="rectangle" name="myParkingLots[]">
-                            <button onclick="${i == 'my' ? 'my' : j.name}Parking()">
+                            <button class="parkingList" onclick="${i == 'my' ? 'my' : j.name}Parking()">
                             <article>
                                 <img class="user" src="${j.img}">
                             </article>
@@ -27,9 +27,33 @@ onload = () => {
             }
             if (i != "friends")
                 document.getElementById(i).innerHTML += `<hr class="line">`;
+
         }
-    });
-}
+    }
+    catch (err) { }
+});
+
+
+carInfo = () => fetch("../json/CarInfo.json").then(info => {
+    return info.json();
+}).then(carInfo => {
+    try {
+        document.getElementById('contentOne').innerHTML =
+            `<div class="close-btn" onclick="togglePopup()">&times;</div>
+            <h1>Car info</h1>
+            <br>
+            <ul id="carInfoPopUp">
+                <li>Car owner: ${carInfo.unknownCar[0].carOwner}</li>
+                <li>Car model: ${carInfo.unknownCar[0].carModel}</li>
+                <li>Color: ${carInfo.unknownCar[0].color}</li>
+                <li>Plate No: ${carInfo.unknownCar[0].plate}</li>
+            </ul>`
+    }
+    catch (err) {
+        console.log(err)
+    }
+
+});
 
 myParking = () => {
     location.href = "parkingPage.html";
@@ -71,12 +95,12 @@ viewElements = (data) => {
     if (data == "Cars") {
         document.getElementById("addedCars").innerHTML = "";
         for (i in carArr)
-            document.getElementById("addedCars").innerHTML += `<article class="carCube">${carArr[i]}<img id="car${i}" src="images/trash.svg" onclick="removeElement(this.id)"></article>`;
+            document.getElementById("addedCars").innerHTML += `<article class="carCube">${carArr[i]}<img class="clickAble" id="car${i}" src="images/trash.svg" onclick="removeElement(this.id)"></article>`;
     }
     else {
         document.getElementById("addedMem").innerHTML = "";
         for (i in memArr)
-            document.getElementById("addedMem").innerHTML += `<article class="memCube">${memArr[i]}<img id="memb${i}" src="images/trash.svg" onclick="removeElement(this.id)"></article>`;
+            document.getElementById("addedMem").innerHTML += `<article class="memCube">${memArr[i]}<img  class="clickAble" id="memb${i}" src="images/trash.svg" onclick="removeElement(this.id)"></article>`;
     }
 }
 
