752f849c12dee1ef240da23a0f14c2f8adc702aa
yuvalBerghaus <markberghaus@outlook.com>
2021-05-02 23:56:51 +0300

    working like crazy

--- js/script.js
+++ js/script.js
@@ -9,25 +9,27 @@ onload = () => {
             for (j of arr[i]) {
                 if (j.name) {
                     document.getElementById(i).innerHTML +=
-                        `<li class="rectangle" name="myParkingLots[]">
+                        `<li class="rectangle" name="myParkingLots[]" id="${j.name == 'lital' ? 'selected' : j.name}">
                             <button onclick="${i == 'my' ? 'my' : j.name}Parking()">
-                            <article>
-                                <img class="user" src="${j.img}">
+                                <article>
+                                    <img class="user" src="${j.img}">
                             </article>
-                            <h2>${i == 'my' ? 'My' : j.name + '\'s'} Parking</h2>
-                            <section class="statusBox">
-                                <article class="trafficLight" id="${j.status}"></article>
-                                <span>${j.status == 'Attention' ? j.status + '!' : j.status}</span>
-                            </section>
+                                    <h2>${i == 'my' ? 'My' : j.name + '\'s'} Parking</h2>
+                                    <section class="statusBox">
+                                        <article class="trafficLight" id="${j.status}"></article>
+                                        <span>${j.status == 'Attention' ? j.status + '!' : j.status}</span>
+                                    </section>
                             </button>
                         </li>`;
                 }
                 else
-                    document.getElementById(i).innerHTML += `<li id="none">NONE</li>`;
+                    document.getElementById(i).innerHTML += `<li id = "none" >NONE</li>`;
             }
             if (i != "friends")
                 document.getElementById(i).innerHTML += `<hr class="line">`;
         }
+        document.getElementById("selected").style.backgroundColor = "#222222"
+        document.getElementById("titleAside").innerHTML += "<span>My parking</span>"
     });
 }
 
@@ -65,14 +67,14 @@ addMember = () => {
     if (document.getElementById("memb").value) {
         memArr[memArr.length] = [document.getElementById("memb").value, document.getElementById("op").value];
         document.getElementById("memb").value = "";
-        document.getElementById("addedMem").innerHTML = `<h6>${memArr}</h6>`;
+        document.getElementById("addedMem").innerHTML = `< h6 > ${memArr}</h6 > `;
     }
 }
 
 viewCars = () => {
     document.getElementById("addedCars").innerHTML = "";
     for (i in carArr)
-        document.getElementById("addedCars").innerHTML += `<article class="carCube">${carArr[i]}<img id="car${i}" src="images/trash.svg" onclick="removeCar(this.id)"></article>`;
+        document.getElementById("addedCars").innerHTML += `< article class="carCube" > ${carArr[i]} <img id="car${i}" src="images/trash.svg" onclick="removeCar(this.id)"></article>`;
 }
 
 removeCar = (index) => {
