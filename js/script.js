108bab03ddb2b08512656356b54b017d7131e49d
Lotem <48315369+LotemY@users.noreply.github.com>
2021-05-04 17:32:39 +0300

    commit fixes

--- js/script.js
+++ js/script.js
@@ -9,21 +9,21 @@ list = () => fetch("../json/users.json").then(res => {
             for (j of arr[i]) {
                 if (j.name) {
                     document.getElementById(i).innerHTML +=
-                        `<li class="rectangle" name = "myParkingLots[]" >
-                        <button class="parkingList" onclick="${i == 'my' ? 'my' : j.name}Parking()">
+                        `<li class="rectangle" name="myParkingLots[]">
+                            <button class="parkingList" onclick="${i == 'my' ? 'my' : j.name}Parking()">
                             <article>
                                 <img class="user" src="${j.img}">
                             </article>
-                                <h2>${i == 'my' ? 'My' : j.name + '\'s'} Parking</h2>
-                                <section class="statusBox">
-                                    <article class="trafficLight" id="${j.status}"></article>
-                                    <span>${j.status == 'Attention' ? j.status + '!' : j.status}</span>
-                                </section>
+                            <h2>${i == 'my' ? 'My' : j.name + '\'s'} Parking</h2>
+                            <section class="statusBox">
+                                <article class="trafficLight" id="${j.status}"></article>
+                                <span>${j.status == 'Attention' ? j.status + '!' : j.status}</span>
+                            </section>
                             </button>
                         </li>`;
                 }
                 else
-                    document.getElementById(i).innerHTML += `<li id = "none"> NONE</li>`;
+                    document.getElementById(i).innerHTML += `<li id="none">NONE</li>`;
             }
             if (i != "friends")
                 document.getElementById(i).innerHTML += `<hr class="line">`;
@@ -32,13 +32,14 @@ list = () => fetch("../json/users.json").then(res => {
         document.getElementById("titleAside").innerHTML += "<span>My parking</span>";
     }
     catch (err) { }
+});
 
-    carInfo = () => fetch("../json/CarInfo.json").then(info => {
-        return info.json();
-    }).then(carInfo => {
-        try {
-            document.getElementById('contentOne').innerHTML =
-                `< div class="close-btn" onclick = "togglePopup()" >& times;</div >
+carInfo = () => fetch("../json/CarInfo.json").then(info => {
+    return info.json();
+}).then(carInfo => {
+    try {
+        document.getElementById('contentOne').innerHTML =
+            `<div class="close-btn" onclick="togglePopup()">&times;</div>
             <h1>Car info</h1>
             <br>
             <ul id="carInfoPopUp">
@@ -47,9 +48,8 @@ list = () => fetch("../json/users.json").then(res => {
                 <li>Color: ${carInfo.unknownCar[0].color}</li>
                 <li>Plate No: ${carInfo.unknownCar[0].plate}</li>
             </ul>`
-        }
-        catch (err) { }
-    })
+    }
+    catch (err) { }
 });
 
 myParking = () => {
@@ -87,21 +87,25 @@ addElement = (arr) => {
     }
 }
 
-addMember = () => {
-    if (document.getElementById("memb").value) {
-        memArr[memArr.length] = [document.getElementById("memb").value, document.getElementById("op").value];
-        document.getElementById("memb").value = "";
-        document.getElementById("addedMem").innerHTML = `< h6 > ${memArr}</h6 > `;
+viewElements = (data) => {
+    if (data == "Cars") {
+        document.getElementById("addedCars").innerHTML = "";
+        for (i in carArr)
+            document.getElementById("addedCars").innerHTML += `<article class="cube">${carArr[i]}<img class="clickAble" id="car${i}" src="images/trash.svg" onclick="removeElement(this.id)"></article>`;
+    }
+    else {
+        document.getElementById("addedMem").innerHTML = "";
+        for (i in memArr)
+            document.getElementById("addedMem").innerHTML += `<article class="cube">${memArr[i]}<img  class="clickAble" id="memb${i}" src="images/trash.svg" onclick="removeElement(this.id)"></article><br>`;
     }
 }
 
-viewCars = () => {
-    document.getElementById("addedCars").innerHTML = "";
-    for (i in carArr)
-        document.getElementById("addedCars").innerHTML += `< article class="carCube" > ${carArr[i]} <img id="car${i}" src="images/trash.svg" onclick="removeCar(this.id)"></article>`;
+removeElement = (index) => {
+    if (!index.split('car')[0]) {
+        carArr.splice(index.split('car')[1], 1);
+        index = "Cars";
+    }
+    else
+        memArr.splice(index.split('memb')[1], 1);
+    viewElements(index);
 }
-
-removeCar = (index) => {
-    carArr.splice(index.split('car')[1], 1);
-    viewCars();
-};
